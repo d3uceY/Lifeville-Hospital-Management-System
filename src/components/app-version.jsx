2344f52848c03f54268980f405cb474604b1996d
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-10-17 18:42:41 +0100

    fix: made github link more responsive

--- src/components/app-version.jsx
+++ src/components/app-version.jsx
@@ -6,12 +6,11 @@ import { Link } from "react-router-dom";
 
 export default function AppVersion() {
   return (
-    <div className="flex items-center gap-4 font-mono">
-      <span className="text-sm text-muted-foreground">App Version</span>
-      <Badge variant="secondary" className="">v{packageJson.version}</Badge>
-      <Link to="https://github.com/d3uceY" target="_blank" className="flex items-center gap-2 hover:underline">
-        <span className="text-sm text-muted-foreground">Made by </span><GithubIcon className="w-4 h-4" /> d3uceY (Jesse)
+    <div className="flex flex-col gap-2 font-mono">
+      <Link to="https://github.com/d3uceY" target="_blank" className="text-xs flex items-center gap-2 hover:underline">
+        <span className="text-muted-foreground">Made by </span><GithubIcon className="w-3 h-3" /> d3uceY (Jesse)
       </Link>
+      <Badge variant="secondary" className="text-xs">v{packageJson.version}</Badge>
     </div>
   );
 }
